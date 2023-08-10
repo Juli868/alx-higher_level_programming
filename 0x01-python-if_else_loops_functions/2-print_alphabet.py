8fae9b32cd4cebbe027b25a46e9db80373daf1d5
Juli868 <bugijul@gmail.com>
2023-08-10 22:37:41 +0200

    Fifth task

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-number = 97;
-for number in range (97, 123):
-        print("{}".format(chr(number)), end = '')
+number = 97
+for number in range(97, 123):
+    print("{}".format(chr(number)), end='')
