260be2df7483688e4adca6be7d6f7c015d65a77d
Juli868 <bugijul@gmail.com>
2023-08-10 22:40:54 +0200

    Fifth task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for character in range(97, 123):
     if (chr(character) != 'e'  and chr(character) != 'q'):
-        print(f"{chr(character)}", end='')
+        print("{}".format(chr(character)), end='')
