e48976d372880ecfdacbfc12ae514b40365342a7
Juli868 <bugijul@gmail.com>
2023-08-11 21:04:24 +0200

    Fist advanced task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,12 +10,12 @@ if (__name__ == '__main__'):
         second = int(sys.argv[3])
         operator = sys.argv[2]
         if (operator == '+'):
-            print('{} {} {} = {}'.format(first, operator, second, add(first, second)))
+            print('{} + {} = {}'.format(first, second, add(first, second)))
         elif(operator == '-'):
-            print('{} {} {} = {}'.format(first, operator, second, sub(first, second)))
+            print('{} - {} = {}'.format(first, second, sub(first, second)))
         elif(operator == '*'):
-            print('{} {} {} = {}'.format(first, operator, second, mul(first, second)))
+            print('{} * {} = {}'.format(first, second, mul(first, second)))
         elif(operator == '/'):
-            print('{} {} {} = {}'.format(first, operator, second, div(first, second)))
+            print('{} / {} = {}'.format(first, second, div(first, second)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
