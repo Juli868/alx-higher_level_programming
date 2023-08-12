8b382bdc71eaf984aee8e4d76a8ca0a080418326
Juli868 <bugijul@gmail.com>
2023-08-12 07:57:54 +0200

    Third task update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 if (__name__ == '__main__'):
     import sys
     counter = len(sys.argv)
-    if (counter == 0):
+    if (counter == 1):
         print("0 arguments.")
-    elif (counter == 1):
-        print("{} argument".format(counter))
+    elif (counter == 2):
+        print("{} argument".format(counter -1))
     else:
-        print("{} arguments:".format(counter - 1))
+        print("{} arguments:".format(counter -1))
     for count in range(1, counter):
         print("{}: {}".format((count), sys.argv[count]))
