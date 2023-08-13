f2a43b8cb66bceab06f9bf8f7e85d9935cfeb1c1
Juli868 <bugijul@gmail.com>
2023-08-13 20:08:33 +0200

    Fifth task:

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     length = int((len(my_list) - 1) / -1)
-    while length < 1:
-        print("{:d}".format(my_list[length]))
-        length += 1
+    for length in range(0, len(my_list)) :
+        print("{:d}".format(my_list[length])) 
