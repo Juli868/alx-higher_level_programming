08078c7955914609b07ef73305313249190002d1
Juli868 <bugijul@gmail.com>
2023-08-15 13:58:17 +0200

    Sixth task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    cpy = my_string
-    for counter in range (0, len(my_string)):
-        if(my_string[counter] == 'c' or my_string[counter] == 'C'):
-           del my_string[counter] 
-        return (my_string)
+    cpy = my_string.replace("c", "")
+    cpy_1 = cpy.replace("C", "")
+    return (cpy_1)
