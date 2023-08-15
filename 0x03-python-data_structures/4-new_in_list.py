c91023c5de2575d1c199e0b9b6083eeeb7daefc7
Juli868 <bugijul@gmail.com>
2023-08-15 12:08:03 +0200

    Fifth task

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = my_list
+    new_list = my_list[:]
     if (idx >= 0 and idx < len(my_list)):
         for counter in range(0, len(my_list)):
             if (idx == counter):
                 new_list[counter] = element
-   return(new_list)
+                return(new_list)
