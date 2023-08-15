347cd4358a38bb244287f67396321c24f5f6e8ed
Juli868 <bugijul@gmail.com>
2023-08-15 11:43:12 +0200

    Third task redo

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    length = int((len(my_list) - 1) / -1)
-    for length in range(0, len(my_list)) :
-        print("{:d}".format(my_list[length])) 
+    length = my_list[::-1]
+    for counter in range(0,len(length)):
+        print("{:d}".format(length[counter]))
---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    for counter in range(0,2):
+        new_t[counter] = tuple_a[counter]+ tuple_b[counter]
+        return (new_t)
