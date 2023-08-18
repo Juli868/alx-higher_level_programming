ae989fc9d51d289d156e520f2cbe667f750f28fc
Juli868 <bugijul@gmail.com>
2023-08-19 01:09:16 +0200

    tenth task

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,5 +3,5 @@ def multiply_by_2(a_dictionary):
     keys_list = list(a_dictionary.keys())
     cpy = a_dictionary.copy()
     for counter in keys_list:
-        cpy[counter] *=  2
+        cpy[counter] *= 2
     return (cpy)
