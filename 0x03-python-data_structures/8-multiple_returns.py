23f9d7234ca77b11b80a0c9df097d734725ad1e9
Juli868 <bugijul@gmail.com>
2023-08-19 00:03:45 +0200

    Following  two tasks

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     answer = [0, None]
-    if (len(my_list != 0)):
+    if (len(sentence) != 0):
         answer[0] = len(sentence)
         answer[1] = sentence[0]
     return(tuple(answer))
