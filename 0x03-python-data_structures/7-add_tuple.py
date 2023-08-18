53ffb23fca9503970f97a0b52d746c5c20ed2182
Juli868 <bugijul@gmail.com>
2023-08-18 20:43:35 +0200

    Eigth task redo

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,9 +3,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
     new_t = [0, 0]
     cp_1 = list(tuple_a)
     cp_2 = list(tuple_b)
-    while (len(cp_1) != 2):
+    while (len(cp_1) <= 2):
         cp_1.append(0)
-    while (len(cp_2) != 2):
+    while (len(cp_2) <= 2):
         cp_2.append(0)
     for counter in range(0,2):
             new_t[counter] = cp_1[counter] + cp_2[counter]
