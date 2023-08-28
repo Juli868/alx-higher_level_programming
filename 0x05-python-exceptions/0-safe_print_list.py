1ec00b6d361f0af07d52b1be3ca6273ef7f5d3fc
Juli868 <bugijul@gmail.com>
2023-08-28 14:38:37 +0200

    First task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,15 +2,12 @@
 def safe_print_list(my_list=[], x=0):
     counter = 0
     i = 0
-    x += 1
-    try:
-        while (my_list[x]):
-            if (x > i):
-                print(my_list[i], end='')
-                counter += 1
-                x += 1
-                i += 1
-    except IndexError:
-        print("")
-    i = 0
+    while (x > i):
+        try:
+            print(my_list[i], end='')
+            counter += 1
+            i += 1
+        except IndexError:
+            break
+    print("")
     return (counter)
