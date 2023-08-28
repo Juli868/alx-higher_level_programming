10221099f236b71a39756c302b877a86ab85531a
Juli868 <bugijul@gmail.com>
2023-08-29 00:47:24 +0200

    First advanced

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,12 +3,10 @@ import sys
 
 
 def safe_print_integer_err(value):
-    r_value = None
     try:
         print("{:d}".format(value))
-        r_value = True
+        return (True)
+
     except(TypeError):
         print("Exception: {}".format(sys.exc_info[1]), file=sys.stderr)
-        r_value = False
-    finally:
-        return(r_value)
+        return (False)
