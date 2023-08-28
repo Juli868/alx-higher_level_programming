a48c4ee0a8a0eefa90198430294c79b5a116adc8
Juli868 <bugijul@gmail.com>
2023-08-28 17:52:29 +0200

    Forth task

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3:
 def safe_print_division(a, b):
-    c = None
     try:
         c = a / b
-    except (UnboundLocalError,ZeroDivisionError):
-        return None
+    except (TypeError, UnboundLocalError, ZeroDivisionError):
+        c = None
     finally:
-        print ("Inside Result: {}".format(c))
+        print("Inside Result: {}".format(c))
         return c
