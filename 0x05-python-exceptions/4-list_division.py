c0d50a9f24aaa257dacb8fab80879065316f1d84
Juli868 <bugijul@gmail.com>
2023-08-29 11:10:54 +0200

    Repo update

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -10,7 +10,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except (TypeError):
             print("wrong type")
             quotient = 0
-        except( IndexError):
+        except (IndexError):
             print("out of range")
             quotient = 0
         finally:
