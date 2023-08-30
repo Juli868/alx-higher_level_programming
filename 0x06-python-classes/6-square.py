5e4fc57ed9b76c0600698f255e6e07f56b53b85f
Juli868 <bugijul@gmail.com>
2023-08-30 14:55:19 +0200

    Seventh task

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Defining objects and instances."""
 
-    def __init__(self, size=0,position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         """Define the objects."""
         self.size = size
         self.position = position
@@ -48,8 +48,8 @@ class Square:
         if (self.__size == 0):
             print("")
         else:
-            [print("") for i in range(0,self.__position[1])]
+            [print("") for i in range(0, self.__position[1])]
             for counter_1 in range(0, self.__size):
-                [print(" ", end='')for counter_2 in range(0, self.__position[0])]
+                [print(" ", end='')for i in range(0, self.__position[0])]
                 [print("#", end='') for j in range(0, self.__size)]
                 print("")
