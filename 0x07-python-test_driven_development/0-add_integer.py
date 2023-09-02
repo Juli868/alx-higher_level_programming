ddbec58d4f13fce3137f02ea9067ee0c8b9a6b7b
Juli868 <bugijul@gmail.com>
2023-09-02 17:32:09 +0200

    First task

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define a function to find a sum of two numbers."""
+
+
+def add_integer(a, b=98):
+    """Describe function details."""
+    if not ((type(a) is int) or (type(a) is float)):
+        raise TypeError("a must be an integer")
+    elif not (type(b) is int or type(b) is float):
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return(a + b)
