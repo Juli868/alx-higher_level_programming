a54e12b9bdabbb2e214530b68574a903dbe48822
Juli868 <bugijul@gmail.com>
2023-09-12 01:16:22 +0200

    Second task redo

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+"""Define class MyList."""
+
+
 class MyList(list):
+    """Instance of list."""
+
     def print_sorted(self):
+        """Sort a list."""
         new_list = self[:]
-        for i in range (len(new_list)):
+        for i in range(len(new_list)):
             if not isinstance(new_list[i], int):
                 raise TypeError("Element must be int")
         print(sorted(new_list))
-
