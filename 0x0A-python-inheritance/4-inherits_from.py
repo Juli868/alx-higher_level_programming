a756f1df1b510b75ea951949c16c28b799b9c5a8
Juli868 <bugijul@gmail.com>
2023-09-12 02:13:07 +0200

    sixth task

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Subclass determination."""
+
+
 def inherits_from(obj, a_class):
-    if issubclass (type(obj), a_class) and type(obj) is not a_class:
+    """Define if there is a subclass provided."""
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     return False
