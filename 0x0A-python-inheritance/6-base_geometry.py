0f4832f532846fd5ea0c075529ba841845886a32
Juli868 <bugijul@gmail.com>
2023-09-12 02:18:14 +0200

    Seventh task

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class."""
+
+
+class BaseGeometry:
+    """class elements."""
+
+    def area(self):
+        """Area calculation."""
+        raise Exception("area() is not implemeted")
