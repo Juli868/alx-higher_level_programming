984dbb7b025c94e1bcc9decb49ec0161fc208a79
Juli868 <bugijul@gmail.com>
2023-09-12 23:22:47 +0200

    Fourth task

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Pay with json."""
+import json
+
+
+def to_json_string(myobj):
+    """Turn t json representation from string.
+
+    @param myobj: string to work on
+    Return: the json representation
+    """
+    return (json.loads(myobj))
