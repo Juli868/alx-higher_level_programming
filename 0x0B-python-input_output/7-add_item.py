914a601f2913493c9dab2928eeae824a09b51b63
Juli868 <bugijul@gmail.com>
2023-09-13 02:35:47 +0200

    Eight task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""Deal with json file."""
 import json
+
+
 __import__('5-save_to_json_file').save_to_json_file
 __import__('6-load_from_json_file').load_from_json_file
-
+try:
+    content = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    content = []
+content.extend(sys.argv[1:])
+save_to_json_file(content, "add_item.json")
