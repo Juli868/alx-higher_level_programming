0d8593fe1b73ef06b0a4d752ef986d3c86fe964a
Juli868 <bugijul@gmail.com>
2023-09-13 15:23:06 +0200

    Eigth task retrial

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -11,10 +11,9 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Set value to the attributes."""
-        if not isinstance(name, str):
-            raise TypeError(f"{name} must be a string")
+        '''if not isinstance(name, str):
+            raise TypeError(f"{name} must be a string")'''
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-
