4b891b6f9a5d43eff51ccb78daf8172bbc1d8e81
Juli868 <bugijul@gmail.com>
2023-09-14 01:03:20 +0200

    First task retrial 5

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """Read content from a file."""
-    with open(filename, 'r') as new:
+    with open(filename, 'r', encoding='UTF-8') as new:
         content = new.read()
-        print(content)
+    print(content)
