99b1dfd7039682be3a4b94401a3fe336050610e7
Juli868 <bugijul@gmail.com>
2023-09-13 02:27:34 +0200

    Seventh task

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""Deal with json files."""
 import json
+
+
 def load_from_json_file(filename):
+    """Build from json file.
+
+    @param filename: name of the json file.
+    """
     with open(filename, 'r') as content:
-        return(json.load(content))
+        return (json.load(content))
