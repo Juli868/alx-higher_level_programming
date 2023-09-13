b513cd9e4223393fd092cb6c959b6f3a11f85994
Juli868 <bugijul@gmail.com>
2023-09-13 15:41:16 +0200

    Nineth task

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -24,4 +24,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """Initialize attributes."""
-        Rectangle.integer_validator
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
