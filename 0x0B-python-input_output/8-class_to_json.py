2bafe75a7a1c34e8f2a91bfdaadbbead2dd2d483
Juli868 <bugijul@gmail.com>
2023-09-14 03:58:22 +0200

    Nineth task

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Deal with json."""
+import json
+
+
+def class_to_json(obj):
+    """Trasnform to json object.
+
+    @param obj: object to deal with.
+    """
+    return (obj.__dict__)
