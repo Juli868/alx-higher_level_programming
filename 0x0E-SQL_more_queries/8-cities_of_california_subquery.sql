a026ed34875de7ede776d684cf0a276894e766cf
Juli868 <bugijul@gmail.com>
2023-10-19 00:03:12 +0200

    Tenth task

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Finding cities in htbn_0d_usa
-SELECT id, name FROM cities where state_id = (SELECT id FROM states WHERE(name = California)) ORDER BY id;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE(name = California)) ORDER BY id ASC;
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,2 @@
+-- displaying all cities in the database
+SELECT cities.id cities.name FROM states INNER JOIN cities ON states.id = cities.id ORDER BY cities.id ASC;
