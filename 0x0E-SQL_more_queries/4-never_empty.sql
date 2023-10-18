737bc13a634764976ce24c7df280e659bb72722b
Juli868 <bugijul@gmail.com>
2023-10-18 16:07:24 +0200

    Sixth task

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,2 @@
+-- Creating a table with a preset id and can't be null
+CREATE TABLE IF NOT EXISTS id_not_null (id INT NOT NULL DEFAULT 1, name VARCHAR(256));
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+-- Creating a table with unique id
+CREATE TABLE unique_id (id INT UNIQUE DEFAULT 1 PRIMARY KEY, name VARCHAR(256));
