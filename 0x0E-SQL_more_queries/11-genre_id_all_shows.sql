d4695579a9e2ee0ebeb1b8757a8efb31aa165eca
Juli868 <bugijul@gmail.com>
2023-10-19 09:46:03 +0200

    Third task

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,2 @@
+-- Displayng all records in  hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- displaying all cities in the database
-SELECT cities.id, cities.name FROM states INNER JOIN cities ON states.id = cities.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name states.name FROM states RIGHT JOIN cities ON states.id = cities.id ORDER BY cities.id ASC;
