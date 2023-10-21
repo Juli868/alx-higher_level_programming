16e2b2800074e695e34c1d235ceb30e6d27df4fa
Juli868 <bugijul@gmail.com>
2023-10-21 13:11:16 +0200

    Tenth task

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,3 @@
 -- Creating a table and adding records.
-CREATE table second_table (id INT, name VARCHAR(256), score INT);
+CREATE table IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT into second_table (id,name,score) VALUES ((1,"JOHN",10),(2,"Alex",3),(3,"Bob",14),(4,"George",8));
