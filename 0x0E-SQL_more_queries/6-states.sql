ec529758eb4e964b701bd1490abcaf90ea2da0c6
Juli868 <bugijul@gmail.com>
2023-11-04 03:17:42 +0200

    Seventh task

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,3 @@
 -- Creating a state table in hbtn_0d_usa
-CREATE DATABASE IF NOT  EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
