15a2b51c007fc5b5e62b634d88fa305485bcc9e0
Juli868 <bugijul@gmail.com>
2023-11-14 01:24:10 +0200

    Third advanced task

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-exports.callMeMoby = function(number, doing){
+exports.callMeMoby = function (number, doing) {
   let i = 0;
   while (i < number) {
     doing();
     i++;
   }
-}
+};
