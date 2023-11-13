a3bd012d8bd03e700ccf51cf40e6f55aff05ffd4
Juli868 <bugijul@gmail.com>
2023-11-14 01:10:03 +0200

    Second advanced task

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,16 +4,15 @@ let second = 0;
 let check = 0;
 let temp;
 let counter = 2;
-while (counter < process.argv.length)
-{
+while (counter < process.argv.length) {
   if (check > first) {
     temp = first;
     first = second;
     second = temp;
   }
   check = parseInt(process.argv[counter]);
-  if (check > second){
-     second = check;
+  if (check > second) {
+    second = check;
   }
   counter++;
 }
