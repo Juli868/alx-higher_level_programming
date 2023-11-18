d0bd85e56bb5c5ac5b31bce6c5f8bdd8cad35c20
Juli868 <Juli868>
2023-11-18 07:31:03 -0800

    Second task

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Script to print all states wtih an N on the beginning in a table named states."""
+if __name__ == '__main__':
+    import MySQLdb as db
+    import sys
+    conn = db.connect(host='localhost', user=sys.argv[1],password=sys.argv[2], database=sys.argv[3])
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM states WHERE states.name='N%'")
+    result = cursor.fetchall()
+    for part in result:
+        print(part)
+    conn.close
