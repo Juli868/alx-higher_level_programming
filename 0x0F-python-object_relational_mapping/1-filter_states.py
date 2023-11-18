8c41948226f2a067aa1f1ff34ba1668b766f885c
Juli868 <Juli868>
2023-11-18 09:43:38 -0800

    Secnd task red

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""Script to print all states wtih an N on the beginning in a table named states."""
+"""Script to print all states wtih an N on the beginning ooooof  states."""
 if __name__ == '__main__':
     import MySQLdb as db
     import sys
-    conn = db.connect(host='localhost', user=sys.argv[1],password=sys.argv[2], database=sys.argv[3])
+    conn = db.connect(
+            host='localhost', user=sys.argv[1],
+            password=sys.argv[2], database=sys.argv[3])
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM states WHERE states.name='N%'")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
     result = cursor.fetchall()
     for part in result:
         print(part)
