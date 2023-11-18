c48c4ac874c1e5a62b7903c91fd0a1e1b7b11d7b
Juli868 <Juli868>
2023-11-18 04:12:16 -0800

    First task checkin'

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-import MySQLdb as db
-conn = db.connect( host='localhost', user='root', password='root', database='hbtn_0e_0_usa')
-cursor = conn.cursor()
-cursor.execute('CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa')
+"""Script to print all info in a table named states."""
+if __name__ == '__main__':
+    import MySQLdb as db
+    conn = db.connect( host='localhost', user=sys.argv[1], password=sys.argv[2], database=sys.atgv[3])
+    cursor = conn.cursor()
+    result = cursor.fetchall('states')
+    for part in result:
+        print(part)
