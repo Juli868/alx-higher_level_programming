c582307825b43c742f6744f2e64ecbec012a186e
Juli868 <Juli868>
2023-11-18 15:07:54 -0800

    Files retouch

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,10 +5,11 @@ if __name__ == '__main__':
     import sys
 
     conn = db.connect(
-            host='localhost', user=sys.argv[1], port=3306
+            host='localhost', user=sys.argv[1], port=3306,
             password=sys.argv[2], database=sys.argv[3])
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE'{}'".format(sys.argv[4]))
+    cursor.execute(
+            "SELECT * FROM states WHERE name LIKE'{}'".format(sys.argv[4]))
     res = cursor.fetchall()
     for result in res:
         print(result)
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
+"""Safe from sql injections."""
 import sys
 import MySQLdb as db
 if __name__ == '__main__':
-   con = db.connect(
-            host='localhost',user=sys.argv[1], port=3306,
-            password=sys.argv[2],database=sys.argv[3])
-   cursor = con.cursor()
-   cursor.execute('SELECT * FROM states WHERE name LIKE %s',(sys.argv[4],))
-   result = cursor.fetchall()
-   for part in result:
-       print(part)
-   con.close()
-
+    con = db.connect(
+            host='localhost', user=sys.argv[1], port=3306,
+            password=sys.argv[2], database=sys.argv[3])
+    cursor = con.cursor()
+    cursor.execute('SELECT * FROM states WHERE name LIKE %s', (sys.argv[4],))
+    result = cursor.fetchall()
+    for part in result:
+        print(part)
+    con.close()
