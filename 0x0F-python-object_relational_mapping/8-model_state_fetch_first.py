56e122752db08783c1341d682a261737f11fb40b
Juli868 <Juli868>
2023-11-20 02:51:02 -0800

    Tasks redo

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,4 +10,4 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
     state = session.query(State).first()
-    print(state.id, state.name, sep=':')
+    print(state.id, state.name, sep=': ')
