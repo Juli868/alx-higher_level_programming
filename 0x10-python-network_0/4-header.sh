0d7bd4472c8d73afa2cef46dd21241f2c6ce96e3
juli868 <bugijul@gmail.com>
2023-12-01 01:36:06 +0200

    last two tasks

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,1 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#send a rquest for a header with "X-School-User-Id" and variable 98 as a header
+curl -sH "X-School-User-Id: 98" "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,1 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#adding a contribution
+curl -sL "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
