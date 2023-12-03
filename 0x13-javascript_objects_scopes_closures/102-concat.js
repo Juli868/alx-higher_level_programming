7ea3bbf33bf5f59257900970f1a85211fd031dba
juli868 <bugijul@gmail.com>
2023-12-03 02:16:22 +0200

    Last advanced task

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 const file = require('fs');
-file.readFile(process.argv[1], 'utf8', (err, data) => {
+file.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
     console.error(err);
   }
   const first = data;
-  file.readFile(process.argv[2], 'utf8', (err, data) => {
+  file.readFile(process.argv[3], 'utf8', (err, data) => {
     if (err) {
       console.error(err);
     }
     const second = data;
     const combined = first + '\n' + second;
-    file.writeFile(process.argv[3], combined, err => {
+    file.writeFile(process.argv[4], combined, err => {
       if (err) {
         console.error(err);
       }
