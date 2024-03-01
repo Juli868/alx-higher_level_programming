a8badcfc54ad443d2904a42d0ba55b066d47fa24
juli868 <bugijul@gmail.com>
2024-03-01 22:53:13 +0200

    No c function.

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for counter in range(0, len(my_string)):
-        cpy[counter] = my_string[counter]
-        if(my_string[counter] == 'c' or my_string[counter] == 'C'):
-            cpy[counter] = ""
-        return (cpy)
+    cpy = ''
+    for part in my_string:
+        if part.lower() != 'c':
+            cpy += part
+    return (cpy)
