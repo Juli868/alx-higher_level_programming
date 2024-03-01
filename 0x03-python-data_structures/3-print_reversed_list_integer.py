9d997773e88d36305a52e9b5b483b879cb961267
juli868 <bugijul@gmail.com>
2024-03-01 22:39:29 +0200

    Handling None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) > 0:
-        length = my_list[::-1]
-        for counter in range(0, len(length)):
-            print("{:d}".format(length[counter]))
-    else:
-        for counter in range(0, len(my_list)):
-            print("{:d}".format(my_list[counter]))
+    if my_list is not None:
+        if len(my_list) > 0:
+            length = my_list[::-1]
+            for counter in range(0, len(length)):
+                print("{:d}".format(length[counter]))
+        else:
+            for counter in range(0, len(my_list)):
+                print("{:d}".format(my_list[counter]))
