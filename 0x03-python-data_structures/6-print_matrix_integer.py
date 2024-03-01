207c393a607930f154304bfce7d4759a2bfe21a8
juli868 <bugijul@gmail.com>
2024-03-01 22:56:13 +0200

    List of lists

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    length_1 = len(matrix)
-    for counter_1 in range(length_1):
-        new = matrix[counter_1]
-        print(new, end = ",")
+    for first in matrix:
+        for second in first:
+             print(second, end = "")
         print("")
